a8b05e77491fb872dcb4390eb76f1cd2dc308105
五十嵐翔 <igara1119@gmail.com>
2023-07-16 23:01:16 +0900

    modify: github pages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   output: 'export',
   assetPrefix: prefixPath,
   basePath: prefixPath,
+  trailingSlash: true,
+  publicRuntimeConfig: {
+    urlPrefix: prefixPath
+  },  
 }
 
 module.exports = nextConfig
