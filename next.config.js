bab622de856757c6467dc157d8290084add87b07
五十嵐翔 <igara1119@gmail.com>
2023-07-16 23:18:41 +0900

    modify: github pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? "/ffmpeg_test'" : undefined,
-  basePath: isProd ? "/ffmpeg_test'" : undefined,
-  trailingSlash: true,
-  publicRuntimeConfig: {
-    urlPrefix: isProd ? "/ffmpeg_test'" : undefined,
-  },  
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
