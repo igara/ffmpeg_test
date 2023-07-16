dabfcdfef8d46bcb2b527dfc78ad600a3fc5ff90
五十嵐翔 <igara1119@gmail.com>
2023-07-16 23:13:42 +0900

    modify: typo

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   assetPrefix: isProd ? "/ffmpeg_test'" : undefined,
-  basePath: isProd ? "/ffmpeg_test'" : undefined,,
+  basePath: isProd ? "/ffmpeg_test'" : undefined,
   trailingSlash: true,
   publicRuntimeConfig: {
     urlPrefix: isProd ? "/ffmpeg_test'" : undefined,
